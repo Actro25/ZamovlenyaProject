f8ca32da67af910f8f3557b0cabb865769291666
DESKTOP-UEGMPNS\Кирило <skiril502@gmail.com>
2024-09-22 11:56:38 +0300

    Any addition

--- ProductService/Controllers/ProductController.cs
+++ ProductService/Controllers/ProductController.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using SharedModels;
 
 namespace ProductService.Controllers
 {
@@ -7,5 +7,25 @@ namespace ProductService.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private static readonly List<Product> Products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Laptop", Price = 1500 },
+            new Product { Id = 2, Name = "Phone", Price = 800 }
+        };
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id)
+        {
+            var product = Products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+                return NotFound();
+            return Ok(product);
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetProducts()
+        {
+            return Ok(Products);
+        }
     }
 }
